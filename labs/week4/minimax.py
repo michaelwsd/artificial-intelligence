ec314b24b6ebde22effbcb37ace13f2016959db3
michaelwsd <michaelwsd@icloud.com>
2026-08-19 22:18:44 +1000

    implemented minimax

--- labs/week4/minimax.py
+++ labs/week4/minimax.py
@@ -52,3 +52,57 @@ transitions = {
     ('O', 'Left'): 'AD',
     ('O', 'Right'): 'AE'
 }
+
+def alpha_beta_search(state):
+    v, move = max_value(state, float('-inf'), float('inf'))
+    return v, move 
+
+def max_value(state, alpha, beta):
+    if is_terminal(state):
+        return outcomes[state], None 
+
+    v, move = float('-inf'), None 
+    for a in ('Left', 'Right'):
+        nxt_state = transitions[(state, a)]
+        v2, _ = min_value(nxt_state, alpha, beta)
+
+        # get the max of the mins
+        if v2 > v:
+            v, move = v2, a 
+            alpha = max(alpha, v) # max we have seen so far
+
+        # beta cut-off 
+        if v2 >= beta:
+            return v, move
+
+    return v, move 
+
+def min_value(state, alpha, beta):
+    if is_terminal(state):
+        return outcomes[state], None
+
+    v, move = float('inf'), None 
+    for a in ('Left', 'Right'):
+        nxt_state = transitions[(state, a)]
+        v2, _ = max_value(nxt_state, alpha, beta)
+
+        # get the min of the maxes
+        if v2 < v:
+            v, move = v2, a 
+            beta = min(beta, v)
+
+        # alpha cut-off 
+        if v2 <= alpha:
+            return v, move 
+
+    return v, move 
+
+def is_terminal(state):
+    if state in outcomes:
+        return True 
+
+    return False 
+
+if __name__ == "__main__":
+    v, move = alpha_beta_search('A')
+    print(v, move)
